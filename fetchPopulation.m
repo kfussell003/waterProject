e3a1345587b507ec51e480ece52706d344b09786
kfussell003 <kbfussell@ucdavis.edu>
2024-05-24 04:09:23 -0700

    Update fetchPopulation.m

--- fetchPopulation.m
+++ fetchPopulation.m
@@ -1,2 +1,6 @@
+load("workingFile.mat")
 url ='https://dof.ca.gov/wp-content/uploads/sites/352/Forecasting/Demographics/Documents/E-1_2024_InternetVersion.xlsx';
-popData = websave('countyPopulations', url); 
+popData = readtable(websave('countyPopulations', url), 'UseExcel',true,'Sheet','E-1 CountyState2024');
+popData = popData(2:59,["Var1", "TotalPopulation"]);
+clear 'url'
+save("workingFile.mat")
